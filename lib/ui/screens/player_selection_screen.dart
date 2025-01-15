8ddad55d2ad33c37275c3ccace64e2530a5e788f
Thomas Carreau <thomas.carreau@reseau.eseo.fr>
2025-01-15 15:41:31 +0100

    refactor: player selection ui

--- lib/ui/screens/player_selection_screen.dart
+++ lib/ui/screens/player_selection_screen.dart
@@ -27,103 +27,149 @@ class _PlayerSelectionScreenState extends State<PlayerSelectionScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Form(
-                  key: _formKey,
-                  child: Row(children: [
-                    Expanded(
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF3d485e),
+                    border: Border.all(
+                      color: Colors.white,
+                      width: 5,
+                    ),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: Form(
+                    key: _formKey,
+                    child: Row(children: <Widget>[
+                      Expanded(
                         child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Player Name',
-                        fillColor: Colors.red,
-                        filled: true,
+                          decoration: InputDecoration(
+                            fillColor: const Color(0xFF3d485e),
+                            labelText: 'Add a player',
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                              borderSide: BorderSide.none,
+                            ),
+                            floatingLabelBehavior: FloatingLabelBehavior.never,
+                          ),
+                          controller: _textFieldController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a name';
+                            }
+                            return null;
+                          },
+                        )
                       ),
-                      controller: _textFieldController,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter a name';
-                        }
-                        return null;
-                      },
-                    )),
-                    IconButton(
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(Colors.blue),
-                      ),
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          context.read<PlayersCubit>().addPlayer(
-                            Players(name: _textFieldController.text)
-                          );
-                          _textFieldController.clear();
-                        }
-                      },
-                      icon: const Icon(Icons.add))
-                  ]),
+                      IconButton(
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all(Colors.white),
+                          iconColor: WidgetStateProperty.all(const Color(0xFF3d485e)),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            context.read<PlayersCubit>().addPlayer(
+                                Players(name: _textFieldController.text)
+                            );
+                            _textFieldController.clear();
+                          }
+                        },
+                        icon: const Icon(Icons.add)
+                      )
+                    ]),
+                  )
                 ),
                 const Divider(),
                 Expanded(
-                  child: ListView.separated(
-                  itemCount: state.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      tileColor: Colors.red,
-                      title: Text(state[index].name),
-                      trailing: IconButton(
-                        style: ButtonStyle(
-                          backgroundColor: WidgetStateProperty.all(Colors.blue),
-                        ),
-                        icon: const Icon(Icons.remove),
-                        onPressed: () {
-                          context.read<PlayersCubit>().removePlayer(state[index]);
-                        }
-                      )
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) {
-                    return const Divider();
-                  },
-                ))
+                  child: Container(
+                    margin: const EdgeInsets.all(30),
+                    child: ListView.builder(
+                      itemCount: state.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.all(20),
+                                child: Text(
+                                  state[index].name,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xFF3d485e),
+                                  )
+                                )
+                              ),
+                              IconButton(
+                                style: ButtonStyle(
+                                  backgroundColor: WidgetStateProperty.all(Colors.pinkAccent),
+                                ),
+                                icon: const Icon(Icons.remove),
+                                onPressed: () {
+                                  context.read<PlayersCubit>().removePlayer(state[index]);
+                                }
+                              )
+                            ]
+                          )
+                        );
+                      }
+                    )
+                  )
+                )
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(Colors.red),
-                ),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Go back to Home')),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all(Colors.green),
-                ),
-                onPressed: () {
-                  if (state.length < 2) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: const Text('Not enough players'),
-                          content: const Text('You need at least 2 players to start the quiz'),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: const Text('OK')
-                            )
-                          ]);
-                      });
-                    return;
-                  }
-                  Navigator.of(context).pushNamed('/quiz-settings');
-                },
-                child: const Text('Start Quiz'))
-            ],
+          Container(
+            margin: const EdgeInsets.all(20), // Set the desired margin
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    shadowColor: WidgetStateProperty.all(Colors.black),
+                    backgroundColor: WidgetStateProperty.all(Colors.pinkAccent),
+                  ),
+                  onPressed: () {
+                    if (state.length < 2) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('Not enough players'),
+                            content: const Text('You need at least 2 players to start the quiz'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('OK')
+                              )
+                            ]);
+                        }
+                      );
+                      return;
+                    }
+                    Navigator.of(context).pushNamed('/quiz-settings');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    child: const Text(
+                      'Start Quiz',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      )
+                    )
+                  )
+                )
+              ],
+            )
           )
         ]);
       })
