1dd3ef708b0da0d6488449dac173798b43929fd9
Thomas Carreau <thomas.carreau@reseau.eseo.fr>
2025-01-16 17:35:44 +0100

    feat: add error message when not connected to internet

--- lib/ui/screens/quiz_settings_screen.dart
+++ lib/ui/screens/quiz_settings_screen.dart
@@ -17,7 +17,6 @@ class QuizSettingsScreen extends StatefulWidget {
 }
 
 class _QuizSettingsScreenState extends State<QuizSettingsScreen> {
-
   final int _maxNumberOfQuestions = 50;
   final int _minNumberOfQuestions = 10;
   final int _stepNumberOfQuestions = 5;
@@ -25,66 +24,93 @@ class _QuizSettingsScreenState extends State<QuizSettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Quiz Settings'),
-      ),
-      body: BlocBuilder<SettingsCubit, Settings>(
-        builder: (context, state) {
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 120),
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-              color: const Color(0xFF3d485e),
-              border: Border.all(
-                color: Colors.white,
-                width: 5,
-              ),
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Form(
-              child: Expanded(
-                child: Column(
+        appBar: AppBar(
+          title: const Text('Quiz Settings'),
+        ),
+        body: BlocBuilder<SettingsCubit, Settings>(
+            builder: (context, settingsState) {
+          return BlocBuilder<CategoryCubit, List<Categories>>(
+              builder: (context, categoryState) {
+            if (categoryState.isEmpty) {
+              return AlertDialog(
+                  title: const Text('Error'),
+                  content: const Text(
+                      'Error while loading categories. Check internet connection.'),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    super.widget));
+                      },
+                      child: const Text('Retry'),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, "/home");
+                        },
+                        child: const Text('Go back to home'))
+                  ]);
+            }
+
+            return Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 120),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF3d485e),
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 5,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Form(
+                    child: Expanded(
+                        child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-
                     // CATEGORY
                     Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF162132),
-                        border: Border.all(
-                          style: BorderStyle.none,
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF162132),
+                          border: Border.all(
+                            style: BorderStyle.none,
+                          ),
+                          borderRadius: BorderRadius.circular(30),
                         ),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: ListTile(
-                        title: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              'Category:',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
+                        child: ListTile(
+                          title: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Text(
+                                'Category:',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                            BlocBuilder<CategoryCubit, List<Categories>>(
-                              builder: (context, state) {
-                                return DropdownSelector(
-                                  items: state.map((category) => category.name).toList(),
+                              DropdownSelector(
+                                  items: categoryState
+                                      .map((category) => category.name)
+                                      .toList(),
                                   onChanged: (String value) {
-                                    state.where((category) => category.name == value).forEach((category) {
-                                      context.read<SettingsCubit>().setCategory(category);
+                                    categoryState
+                                        .where((category) =>
+                                            category.name == value)
+                                        .forEach((category) {
+                                      context
+                                          .read<SettingsCubit>()
+                                          .setCategory(category);
                                     });
-                                  }
-                                );
-                              }
-                            ),
-                          ],
-                        ),
-                      )
-                    ),
+                                  })
+                            ],
+                          ),
+                        )),
 
                     // DIFFICULTY
                     Container(
@@ -110,7 +136,9 @@ class _QuizSettingsScreenState extends State<QuizSettingsScreen> {
                             DropdownSelector(
                               items: difficulties,
                               onChanged: (String? value) {
-                                context.read<SettingsCubit>().setDifficulty(value!);
+                                context
+                                    .read<SettingsCubit>()
+                                    .setDifficulty(value!);
                               },
                             ),
                           ],
@@ -139,24 +167,30 @@ class _QuizSettingsScreenState extends State<QuizSettingsScreen> {
                             ),
                           ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(_minNumberOfQuestions.toString()),
-                              Slider(
-                                value: state.numberOfQuestions.toDouble(),
-                                min: _minNumberOfQuestions.toDouble(),
-                                max: _maxNumberOfQuestions.toDouble(),
-                                divisions: ((_maxNumberOfQuestions - _minNumberOfQuestions) / _stepNumberOfQuestions).round(),
-                                label: state.numberOfQuestions.round().toString(),
-                                onChanged: (double value) {
-                                  context.read<SettingsCubit>().setNumberOfQuestions(value.round());
-                                }
-                              ),
-                              Text(_maxNumberOfQuestions.toString())
-                            ]
-                          ),
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(_minNumberOfQuestions.toString()),
+                                Slider(
+                                    value: settingsState.numberOfQuestions
+                                        .toDouble(),
+                                    min: _minNumberOfQuestions.toDouble(),
+                                    max: _maxNumberOfQuestions.toDouble(),
+                                    divisions: ((_maxNumberOfQuestions -
+                                                _minNumberOfQuestions) /
+                                            _stepNumberOfQuestions)
+                                        .round(),
+                                    label: settingsState.numberOfQuestions
+                                        .round()
+                                        .toString(),
+                                    onChanged: (double value) {
+                                      context
+                                          .read<SettingsCubit>()
+                                          .setNumberOfQuestions(value.round());
+                                    }),
+                                Text(_maxNumberOfQuestions.toString())
+                              ]),
                           Text(
-                            state.numberOfQuestions.toString(),
+                            settingsState.numberOfQuestions.toString(),
                             style: const TextStyle(
                               color: Colors.white,
                               fontSize: 20,
@@ -169,29 +203,25 @@ class _QuizSettingsScreenState extends State<QuizSettingsScreen> {
                     // START QUIZ BUTTON
                     ElevatedButton(
                       style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all(const Color(0xFFF26BFE)),
+                        backgroundColor:
+                            WidgetStateProperty.all(const Color(0xFFF26BFE)),
                       ),
                       onPressed: () {
                         Navigator.pushNamed(context, '/quiz');
                       },
                       child: Container(
-                        padding: const EdgeInsets.all(15),
-                        child: const Text(
-                          'Start Quiz',
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        )
-                      ),
+                          padding: const EdgeInsets.all(15),
+                          child: const Text(
+                            'Start Quiz',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          )),
                     )
                   ],
-                )
-              )
-            )
-          );
-        }
-      )
-    );
+                ))));
+          });
+        }));
   }
 }
